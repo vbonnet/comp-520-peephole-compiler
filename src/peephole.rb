6b0f86d4a3055d514020457901d64baef280236d
Valentin Bonnet <valentinb@sbcglobal.net>
2012-11-17 00:51:23 -0500

    Indenting the tokens because it was getting hard to read the pretty-printed output and determine if it is correct.

--- src/peephole.rb
+++ src/peephole.rb
@@ -29,18 +29,51 @@ end
 #
 # Examples
 #
-#   traverse(tree) do |one, two, ...|
+#   apply(tree) do |one, two, ...|
 #     # body omitted
 #   end
 #
-def traverse(tree, &func)
-  func.call(tree)
-  tree.children.each { |child| traverse(child, &func) } unless tree.empty?
+def apply(tree, &func)
+  traverse(tree, func)
+end
+
+
+# Public: Executes a depth first traversal on a tree object and carries calls two functions for each node. First
+#         |on_enter| is called with the current node as argument as we enter the node (ie before the children are
+#         traversed.  Then |traverse| iterates on all children (in doing so calling |on_enter| and |on_exit| on all
+#         children nodes).  Finally |on_exit| is called on the node.
+#
+# tree     - The ANTLR3::AST::BaseTree object to be traversed.
+# on_enter - The function to call on each node on the way down the tree.
+# on_exit  - The function to call on each node on the way back up the tree.
+#
+# Example
+#
+#   on_enter = lambda do |one, two, ...|
+#     # body omitted
+#   end
+#   on_exit = lambda do |one, two, ...|
+#     # body omitted
+#   end
+#   traverse(tree, on_enter, on_exit)
+#
+def traverse(tree, on_enter, on_exit = nil)
+  on_enter.call(tree)
+  tree.children.each { |child| traverse(child, on_enter, on_exit) } unless tree.empty?
+  on_exit.call(tree) if on_exit != nil
 end
 
 # MAIN
 ARGV.each do |arg|
   parser = Peephole::Parser.new(open(arg))
-  traverse(parser.start.tree) { |node| puts node.text }
+
+  indent = 0
+  print = lambda do |node|
+    s = ' ' * indent
+    indent += 1
+    s << node.text.to_s
+    puts s
+  end
+  traverse(parser.start.tree, print, lambda{ |_|  indent -= 1 })
 end
 
