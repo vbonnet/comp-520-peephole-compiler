0141dd3b0ecb7de4c383fa33592775f9c04e2bf5
Valentin Bonnet <valentinb@sbcglobal.net>
2012-11-16 23:39:13 -0500

    Adding some test code along with a method that traverses and prints an AST.

--- src/peephole.rb
+++ src/peephole.rb
@@ -2,7 +2,7 @@
 
 unless Kernel.respond_to?(:require_relative)
   module Kernel
-    def require_relative( path )
+    def require_relative(path)
       require File.join(File.dirname(__FILE__), path)
     end
   end
@@ -10,9 +10,37 @@ end
 
 require_relative 'grammar/PeepholeParser.rb'
 
-ARGV.each do |a|
-  f = open(a)
-  parser = Peephole::Parser.new( f )
-  start = parser.start
-  puts start
+
+# Internal: Prints all methods that can be called on this objects.  Each of method is ont its own line.
+#
+# obj - the object whose possible methods will be printed
+#
+# sideffect: prints method list to stdout
+#
+def list_methods(obj)
+  puts obj.methods.sort.join("\n").to_s
 end
+
+
+# Public: Executes a depth fist traversal of a tree object and calls a function on each node as it is entered.
+#
+# tree - The ANTLR3::AST::BaseTree object to be traversed.
+# func - The function to call on each node.
+#
+# Examples
+#
+#   traverse(tree) do |one, two, ...|
+#     # body omitted
+#   end
+#
+def traverse(tree, &func)
+  func.call(tree)
+  tree.children.each { |child| traverse(child, &func) } unless tree.empty?
+end
+
+# MAIN
+ARGV.each do |arg|
+  parser = Peephole::Parser.new(open(arg))
+  traverse(parser.start.tree) { |node| puts node.text }
+end
+
